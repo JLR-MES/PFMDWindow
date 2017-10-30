47ec5166329afa73a89a39d70b10a47c06619da9
JLR-MES <janmejay.thakur@tatatechnologies.com>
2017-10-30 17:07:28 +0530

    Added Unit Test

---
+++ BusinessLayer/WinForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class WinForm
+    {
+        public int AddNum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}

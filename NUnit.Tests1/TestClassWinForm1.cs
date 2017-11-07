25ac415eb76f28817f5f45e0db87288d002677b6
JLR-MES <janmejay.thakur@tatatechnologies.com>
2017-11-07 12:15:32 +0530

    z

---
+++ NUnit.Tests1/TestClassWinForm1.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer;
+namespace NUnit.Tests1
+{
+    [TestFixture]
+    public class TestClassWinForm1
+    {
+        [Test]
+        public void AddNum1()
+        {
+            // TODO: Add your test code here
+            int expected, actual;
+            expected = 30;
+            WinForm WinF = new WinForm();
+            actual = WinF.AddNum(10, 20);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void AddNum2()
+        {
+            // TODO: Add your test code here
+            int expected, actual;
+            expected = 40;
+            WinForm WinF = new WinForm();
+            actual = WinF.AddNum(10, 20);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
